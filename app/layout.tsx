a183eecc44146a8f023fa6eb14e565fb04417b83
Sameer kadam <kadamsameer820@gmail.com>
2025-02-16 21:28:46 +0530

    link tag fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,7 @@ export default function RootLayout({
           type="image/png"
           sizes="32x32"
           href="favicon-32x32.png"
+          fetchPriority="auto"
         />
       </head>
       <body
